ef8d172ce2848711c8ef45034fb8d7ba73f80857
John Allen <JALLEN6386@my.sullivan.edu>
2022-06-21 14:08:13 +0900

    Added buttons to form

--- TicTacToe.Designer.cs
+++ TicTacToe.Designer.cs
@@ -35,6 +35,15 @@ namespace TicTacToe
             this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.UxA1 = new System.Windows.Forms.Button();
+            this.UxA2 = new System.Windows.Forms.Button();
+            this.UxA3 = new System.Windows.Forms.Button();
+            this.UxB1 = new System.Windows.Forms.Button();
+            this.UxB2 = new System.Windows.Forms.Button();
+            this.UxB3 = new System.Windows.Forms.Button();
+            this.UxC1 = new System.Windows.Forms.Button();
+            this.UxC2 = new System.Windows.Forms.Button();
+            this.UxC3 = new System.Windows.Forms.Button();
             this.UxMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,7 +54,7 @@ namespace TicTacToe
             this.helpToolStripMenuItem});
             this.UxMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.UxMenuStrip.Name = "UxMenuStrip";
-            this.UxMenuStrip.Size = new System.Drawing.Size(387, 24);
+            this.UxMenuStrip.Size = new System.Drawing.Size(266, 24);
             this.UxMenuStrip.TabIndex = 0;
             this.UxMenuStrip.Text = "menuStrip1";
             // 
@@ -84,15 +93,106 @@ namespace TicTacToe
             this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.aboutToolStripMenuItem.Text = "About";
             // 
+            // UxA1
+            // 
+            this.UxA1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UxA1.Location = new System.Drawing.Point(15, 27);
+            this.UxA1.Name = "UxA1";
+            this.UxA1.Size = new System.Drawing.Size(75, 75);
+            this.UxA1.TabIndex = 1;
+            this.UxA1.UseVisualStyleBackColor = true;
+            // 
+            // UxA2
+            // 
+            this.UxA2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UxA2.Location = new System.Drawing.Point(96, 27);
+            this.UxA2.Name = "UxA2";
+            this.UxA2.Size = new System.Drawing.Size(75, 75);
+            this.UxA2.TabIndex = 2;
+            this.UxA2.UseVisualStyleBackColor = true;
+            // 
+            // UxA3
+            // 
+            this.UxA3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UxA3.Location = new System.Drawing.Point(177, 27);
+            this.UxA3.Name = "UxA3";
+            this.UxA3.Size = new System.Drawing.Size(75, 75);
+            this.UxA3.TabIndex = 3;
+            this.UxA3.UseVisualStyleBackColor = true;
+            // 
+            // UxB1
+            // 
+            this.UxB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UxB1.Location = new System.Drawing.Point(15, 108);
+            this.UxB1.Name = "UxB1";
+            this.UxB1.Size = new System.Drawing.Size(75, 75);
+            this.UxB1.TabIndex = 4;
+            this.UxB1.UseVisualStyleBackColor = true;
+            // 
+            // UxB2
+            // 
+            this.UxB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UxB2.Location = new System.Drawing.Point(96, 108);
+            this.UxB2.Name = "UxB2";
+            this.UxB2.Size = new System.Drawing.Size(75, 75);
+            this.UxB2.TabIndex = 5;
+            this.UxB2.UseVisualStyleBackColor = true;
+            // 
+            // UxB3
+            // 
+            this.UxB3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UxB3.Location = new System.Drawing.Point(177, 108);
+            this.UxB3.Name = "UxB3";
+            this.UxB3.Size = new System.Drawing.Size(75, 75);
+            this.UxB3.TabIndex = 6;
+            this.UxB3.UseVisualStyleBackColor = true;
+            // 
+            // UxC1
+            // 
+            this.UxC1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UxC1.Location = new System.Drawing.Point(15, 189);
+            this.UxC1.Name = "UxC1";
+            this.UxC1.Size = new System.Drawing.Size(75, 75);
+            this.UxC1.TabIndex = 7;
+            this.UxC1.UseVisualStyleBackColor = true;
+            // 
+            // UxC2
+            // 
+            this.UxC2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UxC2.Location = new System.Drawing.Point(96, 189);
+            this.UxC2.Name = "UxC2";
+            this.UxC2.Size = new System.Drawing.Size(75, 75);
+            this.UxC2.TabIndex = 8;
+            this.UxC2.UseVisualStyleBackColor = true;
+            // 
+            // UxC3
+            // 
+            this.UxC3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UxC3.Location = new System.Drawing.Point(177, 189);
+            this.UxC3.Name = "UxC3";
+            this.UxC3.Size = new System.Drawing.Size(75, 75);
+            this.UxC3.TabIndex = 9;
+            this.UxC3.UseVisualStyleBackColor = true;
+            // 
             // TicTacToe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(387, 450);
+            this.ClientSize = new System.Drawing.Size(266, 294);
+            this.Controls.Add(this.UxC3);
+            this.Controls.Add(this.UxC2);
+            this.Controls.Add(this.UxC1);
+            this.Controls.Add(this.UxB3);
+            this.Controls.Add(this.UxB2);
+            this.Controls.Add(this.UxB1);
+            this.Controls.Add(this.UxA3);
+            this.Controls.Add(this.UxA2);
+            this.Controls.Add(this.UxA1);
             this.Controls.Add(this.UxMenuStrip);
             this.MainMenuStrip = this.UxMenuStrip;
             this.Name = "TicTacToe";
             this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tic Tac Toe";
             this.UxMenuStrip.ResumeLayout(false);
             this.UxMenuStrip.PerformLayout();
@@ -109,6 +209,15 @@ namespace TicTacToe
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button UxA1;
+        private System.Windows.Forms.Button UxA2;
+        private System.Windows.Forms.Button UxA3;
+        private System.Windows.Forms.Button UxB1;
+        private System.Windows.Forms.Button UxB2;
+        private System.Windows.Forms.Button UxB3;
+        private System.Windows.Forms.Button UxC1;
+        private System.Windows.Forms.Button UxC2;
+        private System.Windows.Forms.Button UxC3;
     }
 }
 
